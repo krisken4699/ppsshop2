633b47f6a11229a6f373a9f697b6294d319e4e54
Ken <Ken>
2021-04-15 22:18:35 +0700

    what should i name this commit? hahahaha. dont mind me

--- client/src/Home.js
+++ client/src/Home.js
@@ -18,7 +18,7 @@ function Home() {
                 for (i = 0; i < temp.length; i++) {
                     temp[i] = `<option key="${temp[i].charAt(0)}">${temp[i].substring(1)}</option>`
                 }
-                document.getElementById('select-category').innerHTML = '<option>Select Category</option>';
+                document.getElementById('select-category').innerHTML = '<option value="None">Select Category</option>';
                 document.getElementById('select-category').innerHTML += temp;
             })
     }, []);
@@ -35,10 +35,10 @@ function Home() {
             <div className="h-full grid grid-rows-5 grid-cols-6 w-full gap-6">
                 <div className="col-start-1 col-end-7 flex justify-center px-4">
                     <div className="grid grid-cols-3 gap-6 xl:w-50vw w-full">
-                        <div className="col-start-1 relative inline-flex self-center w-min justify-self-end">
-                            <img id="CategoryIcon" src={CategoriesIcon} className="md:inline hidden" alt="React Logo" />
+                        <div className="col-start-1 relative inline-flex self-center w-full justify-self-end">
+                            <img id="CategoryIcon" src={CategoriesIcon} className="justify-self-start md:inline-block hidden" alt="Logo" />
                             <svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M11.7081 0.958965C11.4963 0.748502 11.2099 0.630371 10.9114 0.630371C10.6129 0.630371 10.3265 0.748502 10.1148 0.958965L6.05809 4.95914L2.05791 0.958965C1.84619 0.748502 1.55979 0.630371 1.26127 0.630371C0.962738 0.630371 0.67634 0.748502 0.464621 0.958965C0.358709 1.06401 0.274643 1.18899 0.217275 1.32669C0.159907 1.46439 0.130371 1.61209 0.130371 1.76126C0.130371 1.91043 0.159907 2.05813 0.217275 2.19583C0.274643 2.33353 0.358709 2.45851 0.464621 2.56356L5.25579 7.35473C5.36084 7.46064 5.48582 7.5447 5.62352 7.60207C5.76122 7.65944 5.90892 7.68898 6.05809 7.68898C6.20726 7.68898 6.35496 7.65944 6.49266 7.60207C6.63036 7.5447 6.75534 7.46064 6.86038 7.35473L11.7081 2.56356C11.814 2.45851 11.898 2.33353 11.9554 2.19583C12.0128 2.05813 12.0423 1.91043 12.0423 1.76126C12.0423 1.61209 12.0128 1.46439 11.9554 1.32669C11.898 1.18899 11.814 1.06401 11.7081 0.958965Z" fill="black" /> </svg>
-                            <select onChange={updateSearchIcon} id="select-category" className="link w-min hover:cursor-pointer text-black h-10 bg-transparent pl-5 pr-8 focus:outline-none appearance-none">
+                            <select onChange={updateSearchIcon} id="select-category" className="w-52 link hover:cursor-pointer text-black h-10 bg-transparent pl-5 pr-8 focus:outline-none appearance-none">
                                 <option>Select Category</option>
                             </select>
                         </div>
